985b7ab6ebd8c36f924e2e817a217ee188f35434
Georgeshi98 <georgeshi98@gmail.com>
2021-03-15 18:12:53 -0400

    add some functions
    start by a class with some possible functions that might be useful

---
+++ main.py
@@ -0,0 +1,58 @@
+import pprint
+
+
+class Sudoku:
+    def __init__(self):
+        return
+
+    def random_board(self):
+        """
+        generate a random sudoku game board
+        :return:
+        """
+        return
+
+    def display(self):
+        """
+        display the generated game board
+        :return:
+        """
+        return
+    
+    def take_num_input(self):
+        """
+        function that takes in the user input number
+        :return:
+        """
+        return
+
+    def solve(self):
+        """
+        by pressing space, automatically solve the sudoku
+        :return:
+        """
+        return
+
+    def valid(self):
+        """
+        check if sudoku num is valid with its rules
+        :return:
+        """
+        return
+
+    def find_empty(self):
+        """
+        find the empty spaces in the game
+        :return:
+        """
+        return
+
+    def is_finished(self):
+        """
+        check if the game is finished, no more empty space
+        :return:
+        """
+        return
+
+
+
