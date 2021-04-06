ee6d4136656d9070ac7e03d152349b901c80606f
Georgeshi98 <georgeshi98@gmail.com>
2021-04-06 11:51:05 -0400

    update the check functions

--- main.py
+++ main.py
@@ -47,54 +47,57 @@ class Sudoku:
         """
         return
 
-    def check_row(self, row, num):
+    def check_row(self, pos, num):
         """
         check if the num has already been a part of that row
-        :param row: the row of that num
+        :param pos: the position of that num
         :param num: the input num
         :return: bool, if exists, return True; else, return False
         """
-        for i in range(9):
-            if self.gameBoard[row][i] == num:
+        for i in range(len(self.gameBoard[0])):
+            if self.gameBoard[pos[0]][i] == num and pos[1] != i:
                 return True
         return False
 
-    def check_col(self, col, num):
+    def check_col(self, pos, num):
         """
         check if the num has already been a part of that col
-        :param col: the col of that num
+        :param pos: the position of that num
         :param num: the input num
         :return: bool, if exists, return True; else, return False
         """
-        for i in range(9):
-            if self.gameBoard[col][i] == num:
+        for i in range(len(self.gameBoard)):
+            if self.gameBoard[i][pos[1]] == num and pos[0] != i:
                 return True
         return False
 
-    def check_square(self, row, col, num):
+    def check_square(self, pos, num):
         """
         check if the num has already been a part of that 3x3 square
-        :param row: the row of that num
-        :param col: the col of that num
+        :param pos: the position of that num
         :param num: the input num
         :return: bool, if exists, return True; else, return False
         """
-        for i in range (3):
-            for j in range(3):
-                if self.gameBoard[i + row][j + col] == num:
+        # find which box the num is at
+        box_col = pos[1] // 3
+        box_row = pos[0] // 3
+
+        # loop through all 9 elements in the box to check for duplicates
+        for i in range(box_row * 3, box_row * 3 + 3):
+            for j in range(box_col * 3, box_col * 3 + 3):
+                if self.gameBoard[i][j] == num and (i, j) != pos:
                     return True
         return False
 
     def find_empty(self):
         """
         find the empty spaces in the game
-        :return:
+        :return: a tuple of row and col of that empty space
         """
-        for row in range(9):
-            for col in range(9):
+        for row in range(len(self.gameBoard)):
+            for col in range(len(self.gameBoard[0])):
                 if self.gameBoard[row][col] == 0:
-                    return True
-        return False
+                    return (row, col)
 
     def is_finished(self):
         """
