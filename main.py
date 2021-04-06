c54e879954e12d57ec96b8e034c2cdd575f39075
Georgeshi98 <georgeshi98@gmail.com>
2021-04-06 09:26:50 -0400

    add some basic functions

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pprint
 
+import numpy
+
 
 class Sudoku:
     def __init__(self):
+        self.gameBoard = numpy.matrix()
+
         return
 
-    def random_board(self):
+    def random_board(self, this):
         """
         generate a random sudoku game board
         :return:
@@ -17,7 +21,17 @@ class Sudoku:
         display the generated game board
         :return:
         """
-        return
+        for i in range(len(self.gameBoard)):
+            if i % 3 == 0 and i != 0:
+                print("- - - - - - - - - - - -")
+
+                for j in range(len(self.gameBoard[0])):
+                    if j % 3 == 0 and j != 0:
+                        print(" | ", end="")
+                    if j == 8:
+                        print(self.gameBoard[i][j])
+                    else:
+                        print(str(self.gameBoard[i][j]) + " ", end="")
     
     def take_num_input(self):
         """
@@ -33,26 +47,60 @@ class Sudoku:
         """
         return
 
-    def valid(self):
+    def check_row(self, row, num):
         """
-        check if sudoku num is valid with its rules
-        :return:
+        check if the num has already been a part of that row
+        :param row: the row of that num
+        :param num: the input num
+        :return: bool, if exists, return True; else, return False
         """
-        return
+        for i in range(9):
+            if self.gameBoard[row][i] == num:
+                return True
+        return False
+
+    def check_col(self, col, num):
+        """
+        check if the num has already been a part of that col
+        :param col: the col of that num
+        :param num: the input num
+        :return: bool, if exists, return True; else, return False
+        """
+        for i in range(9):
+            if self.gameBoard[col][i] == num:
+                return True
+        return False
+
+    def check_square(self, row, col, num):
+        """
+        check if the num has already been a part of that 3x3 square
+        :param row: the row of that num
+        :param col: the col of that num
+        :param num: the input num
+        :return: bool, if exists, return True; else, return False
+        """
+        for i in range (3):
+            for j in range(3):
+                if self.gameBoard[i + row][j + col] == num:
+                    return True
+        return False
 
     def find_empty(self):
         """
         find the empty spaces in the game
         :return:
         """
-        return
+        for row in range(9):
+            for col in range(9):
+                if self.gameBoard[row][col] == 0:
+                    return True
+        return False
 
     def is_finished(self):
         """
         check if the game is finished, no more empty space
         :return:
         """
+
         return
-
-
 
